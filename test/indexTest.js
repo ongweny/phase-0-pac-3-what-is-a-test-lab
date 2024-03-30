f6e503885f1855835d015cc7aee5abbe4591eeb9
ongweny <ongwenyjoel@gmail.com>
2024-03-30 19:43:21 +0300

    complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,21 +5,21 @@ const { name, height, message } = require("../index.js");
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+      expect(Name).toEqual("Susan");
     });
   });
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+      expect(Height).toBeMoreThan(0)
+      expect(Height).toBeLessThan(40);
     });
   });
 
   describe("Message", () => {
     it("gives the name and height", () => {
-      expect(message).toInclude(name);
-      expect(message).toInclude(height);
+      expect(Message).toInclude(Name);
+      expect(Message).toInclude(Height);
     });
   });
 });
